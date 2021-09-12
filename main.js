b1e09c36d00e4ad764ec2a8e3fb25624eda46a42
Ishithareddy123 <81242657+Ishithareddy123@users.noreply.github.com>
2021-09-12 11:02:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,26 @@
-menu_list_array = ["Veg Margherita Pizza","Chees Pizza","Chicken Tandoori pizza","Olive Drizzle Pizza","Paprika spice pizza"]
-function getmenu(){
-var htmldata;
- htmldata="<ol class='menulist'>";
- menu_list_array.sort();
- for(var i=0;i<menu_list_array.length;i++){
- htmldata=htmldata+"<li>"+menu_list_array[i]+"</li>";
-//Complete the code
+canvas= document.getElementById("my_Canvas");
+ctx=canvas.getContext("2d");
+mouseEvent="";
+var radius=20;
+var colour="black";
+var width=3;
+canvas.addEventListener("mousedown",mymousedown);
+function mymousedown(e){
+mouseEvent="mousedown";
+colour=document.getElementById("co").value;
+width=document.getElementById("wi").value;
+radius=document.getElementById("ra").value;
 }
-
-htmldata=htmldata+"</ol>"
-document.getElementById("display_menu").innerHTML=htmldata;
-}
-function add_item(){
-var htmldata="";
-
-//Complete the code
-menu_list_array.sort();
-for( var i=0;i<menu_list_array.length;i++){
-
-
-htmldata="<section class='card'>";
-//htmldata=htmldata+"<li>"+menu_list_array[i]+"</li>"
-    htmldata=htmldata+"<div class='card'>"
-    +"<img src='pizzaImg.png'>"
-    +menu_list_array[i]+"</div>"; 
-
-htmldata=htmldata+"</section>";
-console.log(htmldata);
-}
-document.getElementById("display_addedmenu").innerHTML=htmldata;
-}
-function add_top(){
-//Complete the code
-var item=document.getElementById("add_item").value;
-menu_list_array.push(item);
-
-menu_list_array.sort();
-}
+canvas.addEventListener("mousemove",mymousemove);
+function mymousemove(e){
+currentpofX=e.clientX-canvas.offsetLeft;
+currentpofY=e.clientY-canvas.offsetTop;
+if(mouseEvent=="mousedown")
+{
+    
+    ctx.beginPath();
+    ctx.strokeStyle=colour;
+    ctx.lineWidth=width;
+    ctx.arc(currentpofX,currentpofY,radius,0,2*Math.PI);
+    ctx.stroke();
+}}
