3a86630b4484fe592017749e8130bc9b19cea0c0
Ishithareddy123 <81242657+Ishithareddy123@users.noreply.github.com>
2021-09-12 10:40:17 +0530

    Add files via upload

--- main-kvishnu-7390.js
+++ main-kvishnu-7390.js
@@ -1,26 +1,25 @@
-canvas= document.getElementById("my_Canvas");
+canvas= document.getElementById("my_canvas");
 ctx=canvas.getContext("2d");
-mouseEvent="";
-var radius=20;
-var colour="black";
-var width=3;
+var colour=document.getElementById("co").innerHTML;
+var width=document.getElementById("wi").innerHTML;
+var radius=document.getElementById("ra").innerHTML;
 canvas.addEventListener("mousedown",mymousedown);
 function mymousedown(e){
 mouseEvent="mousedown";
 colour=document.getElementById("co").value;
 width=document.getElementById("wi").value;
-radius=document.getElementById("ra").value;
+radus=document.getElementById("ra").value;
 }
-canvas.addEventListener("mousemove",mymousemove);
+
 function mymousemove(e){
 currentpofX=e.clientX-canvas.offsetLeft;
 currentpofY=e.clientY-canvas.offsetTop;
 if(mouseEvent=="mousedown")
 {
-    
+   
     ctx.beginPath();
     ctx.strokeStyle=colour;
     ctx.lineWidth=width;
-    ctx.arc(currentpofX,currentpofY,radius,0,2*Math.PI);
+    clientX.arc(currentpofX,currentpofY,radius,0,2*Math.PI);
     ctx.stroke();
 }}
